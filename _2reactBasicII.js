95a5862e807d151a7f17e67d75eabbe848e1963f
Atul Oli <161449224+27-Atul@users.noreply.github.com>
2024-04-28 09:15:20 +0530

    Add files via upload

--- _2reactBasicII.js
+++ _2reactBasicII.js
@@ -1,20 +1,18 @@
-// most important and costly thing in React or in browser is that 
-// DOM nodes need to be manipulate(change continuously or update or hide the changes) 
+// most important and costly thing in React or in browser is that
+// DOM nodes need to be manipulate(change continuously or update or hide the changes)
 
 // how we can give attributes to our tag
-// it create an object only 
+// it create an object only
 const heading = React.createElement(
-    "h1",
-    {id: "heading"  },   // or
-    // {id: "heading" , xyz:"abc"},
-    "Hlw this is my first React code: "
+  "h1",
+  { id: "heading" }, // or
+  // {id: "heading" , xyz:"abc"},
+  "Hlw this is my first React code: "
 );
 
 // console.log(heading);   //in console we see that the object // a react element--->normally a js object...
 const root = ReactDOM.createRoot(doucment.getElementById("root"));
-root.render(heading);    // render function/method  is converting this object into  create that h1 tag and put that inside the root
-
-
+root.render(heading); // render function/method  is converting this object into  create that h1 tag and put that inside the root
 
 // II method
 /*
@@ -28,12 +26,15 @@ ReactElement(object)=> HTML(Browser Understands)
 */
 // to write this code in react or in JS
 const parent = React.createElment(
+  "div",
+  { id: "parent" },
+  React.createElment(
     "div",
-    {id :"parent"},
-    React.createElment("div",{id :"child"},
-    React.createElement("h1", {}, "this is my h1 tag"))
+    { id: "child" },
+    React.createElement("h1", {}, "this is my h1 tag")
+  )
 );
 
-console.log(parent);          //object
-const root1 = ReactDOM.createRoot(document.getElementById("roots"));
+console.log(parent); //object
+const root1 = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
--- _3parentInside2child.js
+++ _3parentInside2child.js
@@ -10,17 +10,20 @@
 </div>
  */
 
-// to do this in react 
+// to do this in react using js which is known as JSX
 
-const heading = React.createElement("div",{id:"parent"},[
-    react.createElement("div",{id : "child1"},[
-        React.createElement("h1",{},"hi i am h1 first child"),
-        React.createElement("h2",{},"hi i am h2 first child"),
-    ]),
-    react.createElement("div",{id : "child2"},[
-        React.createElement("h1",{},"hi i am h1 second child"),
-        React.createElement("h2",{},"hi i am h2 second child"),
-    ]),
+const heading = React.createElement("div", { id: "parent" }, [
+  react.createElement("div", { id: "child1" }, [
+    React.createElement("h1", {}, "hi i am h1 first child"),
+    React.createElement("h2", {}, "hi i am h2 first child"),
+  ]),
+  react.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "hi i am h1 second child"),
+    React.createElement("h2", {}, "hi i am h2 second child"),
+  ]),
 ]);
 
 //JSX SOMETHING which help written this complex code of react ... use for any tag in react and make this life easier
+console.log(heading); //object
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
